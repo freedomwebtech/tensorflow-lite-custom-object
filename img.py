487912f49a077d5eb63e7a760af0bcf45414f2ea
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2022-08-28 16:03:13 +0530

    Update img.py

--- img.py
+++ img.py
@@ -7,7 +7,7 @@ cap=cv2.VideoCapture(0)
 
 while cpt < maxFrames:
     ret, frame = cap.read()
-    frame=cv2.resize(flipped,(640,480))
+    frame=cv2.resize(frame,(640,480))
     cv2.imshow("test window", frame) # show image in window
     cv2.imwrite("/home/pi/images/A_%d.jpg" %cpt, frame)
     time.sleep(0.5)
